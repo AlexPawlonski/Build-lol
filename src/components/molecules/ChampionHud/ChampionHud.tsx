7ff25cc9231e89777f486eb93e3741d402e98f5b
Alex <apawlonski8@gmail.com>
2023-11-08 16:29:58 +0100

    update style champion hud and add state per lvl to champ stats

--- src/components/molecules/ChampionHud/ChampionHud.tsx
+++ src/components/molecules/ChampionHud/ChampionHud.tsx
@@ -1,36 +1,28 @@
 import { ReactElement } from "react";
-import { Champion } from "../../../interface";
-import { Bar, Level, Passive, Spell } from "../../atoms";
+import { Champion, ChampionStats, PerLvlStats } from "../../../interface";
+import { Bar, Level } from "../../atoms";
 
 export interface Props {
   champSelected: Champion;
+  champStats: ChampionStats;
+  champPerLvl: PerLvlStats;
 }
 
-const ChampionHud = ({ champSelected }: Props): ReactElement => {
-  const stats = champSelected.stats;
-  const spells = champSelected.spells;
-  const passive = champSelected.passive;
-
+const ChampionHud = ({ champSelected, champStats, champPerLvl }: Props): ReactElement => {
   return (
     <section className="w-full flex flex-col gap-4">
-      <div className="flex justify-between">
-        <h1>{champSelected.name}</h1>
-        <Level onChange={(lvl) => console.log(lvl)} />
+      <div className="flex justify-between items-center">
+        <h2 className="beaufortforLOL text-2xl text-or-3 uppercase">{champSelected.name}</h2>
+        <Level champSelectStats={champSelected.stats} champPerLvl={champPerLvl} />
       </div>
       <div className="flex flex-col gap-4">
-        <Bar type="hp" state={stats.hp} />
-        {stats.mp !== 0 && <Bar type={stats.mpperlevel === 0 ? "energie" : "mp"} state={stats.mp} />}
-      </div>
-      <div className="flex gap-4">
-        <Passive passive={passive} />
-        {spells.map((spell, key) => (
-          <Spell key={key} spell={spell} />
-        ))}
+        <Bar type="hp" state={champStats.hp} />
+        {champStats.mp !== 0 && (
+          <Bar type={champSelected.stats.mpperlevel === 0 ? "energie" : "mp"} state={champStats.mp} />
+        )}
       </div>
     </section>
   );
 };
 
 export default ChampionHud;
-
-
